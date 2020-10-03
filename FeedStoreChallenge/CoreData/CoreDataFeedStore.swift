74807960443bcc055c0307bfce28ceca016be8b5
Erik Agujari <erikagujari@gmail.com>
2020-10-03 13:38:10 +0200

    implement retrieve method on `CoreDataFeedStore`

--- FeedStoreChallenge/CoreData/CoreDataFeedStore.swift
+++ FeedStoreChallenge/CoreData/CoreDataFeedStore.swift
@@ -5,10 +5,14 @@
 //  Created by Erik Agujari on 03/10/2020.
 //  Copyright © 2020 Essential Developer. All rights reserved.
 //
-import Foundation
+import CoreData
 
 public struct CoreDataFeedStore: FeedStore {
-    public init() {}
+    let context: NSManagedObjectContext?
+    
+    public init() {
+        context = CoreDataFeedStore.container()?.newBackgroundContext()
+    }
     
     public func deleteCachedFeed(completion: @escaping DeletionCompletion) {
         
@@ -19,6 +23,35 @@ public struct CoreDataFeedStore: FeedStore {
     }
     
     public func retrieve(completion: @escaping RetrievalCompletion) {
-        completion(.empty)
+        guard let context = context,
+              let fetch = try? context.fetch(CoreDataFeed.fetchRequest()) as? [CoreDataFeed]
+        else {
+            completion(.failure(CoreDataError.retrieveError))
+            return
+        }
+        
+        guard let coreDataFeed = fetch.first,
+              !coreDataFeed.images.isEmpty
+        else {
+            completion(.empty)
+            return
+        }
+        
+        completion(.found(feed: coreDataFeed.images.compactMap { $0.toLocalFeedImage() }, timestamp: coreDataFeed.timestamp))
+    }
+}
+
+private extension CoreDataFeedStore {
+    static func container() -> NSPersistentContainer? {
+        let fileName = "CoreDataFeed"
+        guard let url = Bundle(for: CoreDataFeed.self).url(forResource: fileName, withExtension: "momd"),
+              let managedObjectModel = NSManagedObjectModel(contentsOf: url)
+        else { return nil }
+        
+        return NSPersistentContainer(name: fileName, managedObjectModel: managedObjectModel)
+    }
+    
+    enum CoreDataError: Error {
+        case retrieveError
     }
 }
